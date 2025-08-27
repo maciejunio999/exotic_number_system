ee7a7a893549bce718dfff7f36ee81f8e392bebb
Maciej Antosz <maciejantosz13@gmail.com>
2025-08-27 22:44:23 +0200

    test octal conversion and choose best one

--- converters/oct_decimal.py
+++ converters/oct_decimal.py
@@ -1,2 +1,5 @@
-def oct_to_decimal(x, y):
-    print('oct_to_decimal...')
+def octal_to_decimal(octal):
+    if octal == 0:
+        return 0
+    else:
+        return (octal % 10) + 8 * octal_to_decimal(octal // 10)
