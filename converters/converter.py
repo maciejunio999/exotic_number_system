f298e68e3299ea8f7570d98e9186abbe4e65be90
Maciej Antosz <maciejantosz13@gmail.com>
2025-09-07 23:49:52 +0200

    create converter handler

--- converters/converter.py
+++ converters/converter.py
@@ -1,2 +1,15 @@
-def conversion():
-    print('converting...')
+from hex_decimal import hex_to_decimal
+from binary_decimal import binary_to_decimal
+from oct_decimal import octal_to_decimal
+from roman_decimal import roman_to_decimal
+
+def conversion(coversion, number):
+    match coversion:
+        case 'hex_dec':
+            return hex_to_decimal(number)
+        case 'bin_dec':
+            return binary_to_decimal(number)
+        case 'oct_dec':
+            return octal_to_decimal(int(number))
+        case 'rom_dec':
+            return roman_to_decimal(number)
